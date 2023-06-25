1b51569a969ffb798e57c3dff8fa2812a9036128
Patrick Gietl <patrick.gietl@it4ng.eu>
2023-06-25 13:54:31 +0200

    adds ble-report, and neo-callbacks #7

--- src/comm/ble/ble_demo.py
+++ src/comm/ble/ble_demo.py
@@ -6,15 +6,19 @@ from . import ble_advertising
 
 _IRQ_CENTRAL_CONNECT = const(1)
 _IRQ_CENTRAL_DISCONNECT = const(2)
+_IRQ_GATTS_WRITE = const(3)
 _IRQ_GATTS_INDICATE_DONE = const(20)
 
 _FLAG_READ = const(0x0002)
 _FLAG_NOTIFY = const(0x0010)
 _FLAG_INDICATE = const(0x0020)
 
+_FLAG_WRITE_NO_RESPONSE = const(0x0004)
+_FLAG_WRITE = const(0x0008)
+
 # Characteristics
 # org.bluetooth.characteristic.report
-_REP_CHAR = (bluetooth.UUID(0x2A4D), _FLAG_READ | _FLAG_NOTIFY | _FLAG_INDICATE,)
+_REP_CHAR = (bluetooth.UUID(0x2A4D), _FLAG_WRITE_NO_RESPONSE | _FLAG_WRITE,)
 # org.bluetooth.characteristic.luminous_intensity
 _LUM_CHAR = (bluetooth.UUID(0x2B01), _FLAG_READ | _FLAG_NOTIFY | _FLAG_INDICATE,)
 # org.bluetooth.characteristic.humidity
@@ -23,27 +27,34 @@ _HUM_CHAR = (bluetooth.UUID(0x2A6F), _FLAG_READ | _FLAG_NOTIFY | _FLAG_INDICATE,
 _TOU_CHAR = (bluetooth.UUID(0x2A6D), _FLAG_READ | _FLAG_NOTIFY | _FLAG_INDICATE,)
 # org.bluetooth.characteristic.battery_energy_status
 _BAT_CHAR = (bluetooth.UUID(0x2BF0), _FLAG_READ | _FLAG_NOTIFY | _FLAG_INDICATE,)
+# org.bluetooth.characteristic.light_output
+_NEO_CHAR = (bluetooth.UUID(0x2BE2), _FLAG_WRITE_NO_RESPONSE | _FLAG_WRITE,)
 
 # Services
 # org.bluetooth.service.environmental_sensing
 _ENV_SENSE_UUID = bluetooth.UUID(0x181A)
 # org.bluetooth.service.battery
 _BAT_UUID = bluetooth.UUID(0x180F)
+# org.bluetooth.service.generic_media_control
+_GME_UUID = bluetooth.UUID(0x1849)
 
 _ENV_SENSE_SERVICE = (_ENV_SENSE_UUID, (_LUM_CHAR, _HUM_CHAR, _TOU_CHAR, _REP_CHAR,),)
 _BAT_SERVICE = (_BAT_UUID, (_BAT_CHAR,),)
-_SERVICES = (_ENV_SENSE_SERVICE, _BAT_SERVICE,)
+_GME_SERVICE = (_GME_UUID, (_NEO_CHAR,),)
+_SERVICES = (_ENV_SENSE_SERVICE, _BAT_SERVICE, _GME_SERVICE,)
 # org.bluetooth.characteristic.gap.appearance.xml
 _ADV_APPEARANCE_GENERIC_LIGHT_SOURCE = const(1984)
 
 
 class BleSmartLeaf:
-    def __init__(self, ble, name="smart-leaf-ble"):
+    def __init__(self, ble, callback_rep=None, callback_neo=None, name="smart-leaf-ble"):
         self.connected = False
         self._ble = ble
         self._ble.active(True)
         self._ble.irq(self._irq)
-        ((self._lum_handle, self._hum_handle, self._tou_handle, self._rep_handle,), (self._bat_handle,), ) = self._ble.gatts_register_services(_SERVICES)
+        self._callback_rep = callback_rep
+        self._callback_neo = callback_neo
+        ((self._lum_handle, self._hum_handle, self._tou_handle, self._rep_handle,), (self._bat_handle,), (self._neo_handle,), ) = self._ble.gatts_register_services(_SERVICES)
         self._connections = set()
         self._payload = ble_advertising.advertising_payload(
             name=name, services=[_ENV_SENSE_UUID, _BAT_UUID], appearance=_ADV_APPEARANCE_GENERIC_LIGHT_SOURCE
@@ -55,28 +66,51 @@ class BleSmartLeaf:
         if event == _IRQ_CENTRAL_CONNECT:
             conn_handle, _, _ = data
             self._connections.add(conn_handle)
-            print('BLE Device connected to ESP32')
+            self._log('Central connected to ESP32', '(handle: ' + str(conn_handle) + ')')
             self.connected = True
         elif event == _IRQ_CENTRAL_DISCONNECT:
             conn_handle, _, _ = data
             self._connections.remove(conn_handle)
             # Start advertising again to allow a new connection.
             self._advertise()
-            print('BLE Device disconnected to ESP32')
+            self._log('Central disconnected from ESP32', '(handle: ' + str(conn_handle) + ')')
             self.connected = False
         elif event == _IRQ_GATTS_INDICATE_DONE:
             conn_handle, value_handle, status = data
+        elif event == _IRQ_GATTS_WRITE:
+            conn_handle, value_handle = data
+            self._log('Central write received', '(handle: ' + str(conn_handle) + ')', '(value-handle: ' + str(value_handle) + ')')
+            if value_handle == self._rep_handle:
+                upckd = struct.unpack('<i', self._ble.gatts_read(value_handle))[0]
+                self._log('Report value received:', upckd)
+                if self._callback_rep is not None:
+                    self._callback_rep(upckd)
+            if value_handle == self._neo_handle:
+                upckd = struct.unpack('IHHH', self._ble.gatts_read(value_handle))
+                # First value are NEO-LED indexes e.g. 100000000 which means no LED is written to
+                # e.g. 111100000 which means first three LEDs are written to
+                # e.g. 211100000 which means first three LEDs are written to and other LED-values are not cleared
+                # Second, Third, Fourth values are unsigned RGB-shorts e.g. (255, 0, 0) => RED
+                # e.g. (255, 255, 255) => White
+                # e.g. (128, 128, 128) => 50% Grey, and so on
+                self._log('Neo value received:', upckd)
+                if self._callback_neo is not None:
+                    self._callback_neo(upckd)
 
     def set_light_value(self, light_value, notify=False, indicate=False):
+        self._log('Writing Luminosity value:', light_value)
         self.set_value(self._lum_handle, 'd', light_value, notify, indicate)
 
     def set_humidity_value(self, humidity_value, notify=False, indicate=False):
+        self._log('Writing Humidity value:', humidity_value)
         self.set_value(self._hum_handle, 'd', humidity_value, notify, indicate)
 
     def set_battery_value(self, battery_value, notify=False, indicate=False):
+        self._log('Writing Battery value:', battery_value)
         self.set_value(self._bat_handle, '<i', battery_value, notify, indicate)
 
     def set_touch_value(self, touch_value, notify=False, indicate=False):
+        self._log('Writing Touch value:', touch_value)
         self.set_value(self._tou_handle, '<i', touch_value, notify, indicate)
 
     def set_value(self, handle, struct_fmt, value, notify=False, indicate=False):
@@ -85,9 +119,11 @@ class BleSmartLeaf:
             for conn_handle in self._connections:
                 if notify:
                     # Notify connected centrals.
+                    self._log('Trying to notify central', '(handle: ' + str(conn_handle) + ')')
                     self._ble.gatts_notify(conn_handle, handle)
                 if indicate:
                     # Indicate connected centrals.
+                    self._log('Trying to indicate central', '(handle: ' + str(conn_handle) + ')')
                     self._ble.gatts_indicate(conn_handle, handle)
 
     def get_mac_address(self):
@@ -102,3 +138,9 @@ class BleSmartLeaf:
 
     def _advertise(self, interval_us=500000):
         self._ble.gap_advertise(interval_us, adv_data=self._payload)
+
+    def _log(self, *values: object):
+        a = list(values)
+        a.insert(0, '[BLE]')
+        values = tuple(a)
+        print(*values)
--- src/tests.py
+++ src/tests.py
@@ -11,12 +11,12 @@ import bluetooth
 
 def test():
     print('Starting Tests...')
-    test_generic('Light Sense', test_sense_light)
-    test_generic('Humidity Sense', test_sense_humidity)
-    test_generic('Touch Sense', test_sense_touch)
-    test_generic('LED Display', test_display_neo)
-    test_generic('BLE-Comm', lambda: test_ble_comm(True, True, False, True))
-    test_generic('Game Guess Waterlevel', test_game_guess_waterlevel)
+    #test_generic('Light Sense', test_sense_light)
+    #test_generic('Humidity Sense', test_sense_humidity)
+    #test_generic('Touch Sense', test_sense_touch)
+    #test_generic('LED Display', test_display_neo)
+    test_generic('BLE-Comm', lambda: test_ble_comm(False, False, False, False))
+    #test_generic('Game Guess Waterlevel', test_game_guess_waterlevel)
     print('Ending Tests')
 
 
@@ -30,40 +30,50 @@ def test_generic(name, test_func):
 
 
 def test_ble_comm(notify_lum=False, notify_hum=False, notify_bat=False, notify_touch=False):
-    tp_ble_indicator = neo_onboard.NeoBlue()
-    tp_ble = ble_demo.BleSmartLeaf(bluetooth.BLE())
+    try:
+        tp_ble_indicator = neo_onboard.NeoBlue()
+        tp_ble = ble_demo.BleSmartLeaf(bluetooth.BLE())
 
-    print('BLE-Address is: ', tp_ble.get_mac_address())
-    while not tp_ble.connected:
-        tp_ble_indicator.refresh_emit()
-        time.sleep_ms(10)
+        print('BLE-Address is: ', tp_ble.get_mac_address())
+        while not tp_ble.connected:
+            tp_ble_indicator.refresh_emit()
+            time.sleep_ms(10)
 
-    time.sleep(5)
-    if notify_lum:
-        sense_light = light.SenseLight(Pin(14), Pin(32), i2c_bus_idx=0)
-        value = sense_light.read()
-        print('trying to send luminosity data:', value)
-        tp_ble.set_light_value(value, True)
+        if not notify_lum and not notify_hum and not notify_bat and not notify_touch:
+            return True
 
-    time.sleep(5)
-    if notify_hum:
-        sense_humidity = humidity.SenseHumidity(Pin(15), Pin(33), i2c_bus_idx=1)
-        value = sense_humidity.read_moisture()
-        print('trying to send humidity data:', value)
-        tp_ble.set_humidity_value(value, True)
+        time.sleep(5)
+        if notify_lum:
+            sense_light = light.SenseLight(Pin(14), Pin(32), i2c_bus_idx=0)
+            value = sense_light.read()
+            print('trying to send luminosity data:', value)
+            tp_ble.set_light_value(value, True)
 
-    if notify_touch:
-        sense_touch = touch.SenseTouch(Pin(12), touch_threshold=150)
-        for i in range(350):
-            value = sense_touch.read()
-            if value == touch.SenseTouch.SenseTouchValueEnum.SHORT:
-                print('Short Touch')
-                tp_ble.set_touch_value(0, True)
-            elif value == touch.SenseTouch.SenseTouchValueEnum.LONG:
-                print('Long Touch')
-                tp_ble.set_touch_value(1, True)
+        time.sleep(5)
+        if notify_hum:
+            sense_humidity = humidity.SenseHumidity(Pin(15), Pin(33), i2c_bus_idx=1)
+            value = sense_humidity.read_moisture()
+            print('trying to send humidity data:', value)
+            tp_ble.set_humidity_value(value, True)
 
-            time.sleep(0.1)
+        if notify_touch:
+            sense_touch = touch.SenseTouch(Pin(12), touch_threshold=150)
+            for i in range(350):
+                value = sense_touch.read()
+                if value == touch.SenseTouch.SenseTouchValueEnum.SHORT:
+                    print('Short Touch')
+                    tp_ble.set_touch_value(0, True)
+                elif value == touch.SenseTouch.SenseTouchValueEnum.LONG:
+                    print('Long Touch')
+                    tp_ble.set_touch_value(1, True)
+
+                time.sleep(0.1)
+
+        return True
+
+    except Exception as e:
+        print(e)
+        return False
 
 
 def test_game_guess_waterlevel():
@@ -93,7 +103,8 @@ def test_sense_light():
             time.sleep(0.5)
         return True
 
-    except:
+    except Exception as e:
+        print(e)
         return False
 
 
@@ -108,7 +119,8 @@ def test_sense_humidity():
             time.sleep(0.5)
         return True
 
-    except:
+    except Exception as e:
+        print(e)
         return False
 
 
@@ -125,7 +137,8 @@ def test_sense_touch():
             time.sleep(0.1)
         return True
 
-    except:
+    except Exception as e:
+        print(e)
         return False
 
 
@@ -140,7 +153,8 @@ def test_display_neo():
         neo.clear()
         return True
 
-    except:
+    except Exception as e:
+        print(e)
         return False
 
 
